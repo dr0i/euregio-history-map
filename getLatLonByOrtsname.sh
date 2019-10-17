0b9aab0f4f57f96e6f07cfe3409db60ca5c015f6
Pascal Christoph <christoph@hbz-nrw.de>
2019-10-17 17:08:39 +0200

    WIP properties list, hashmap in bash?

--- getLatLonByOrtsname.sh
+++ getLatLonByOrtsname.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Lookup lobid/nwbib by an Ortsname and get its lat and lon
 # date: 2019-10-14
 # author: dr0i
@@ -5,23 +6,16 @@ echo "ort, wd, latitude,longitude" > ortsnameWDLatLon.txt
 echo '{
   "type": "FeatureCollection",
   "features": [
-' > ortsnameWDLatLon.geo.json
-getLatLonByOrtViaLobid () {
-IFS=\
-;
-ORT=$1
-DATA=$(curl "https://lobid.org/resources/search?q=spatial.label%3A%22$ORT%22+AND+inCollection.id%3A%22http%3A%2F%2Flobid.org%2Fresources%2FHT014176012%23!%22&from=1&size=1&format=json" 2>/dev/null)
-LAT=$(echo $DATA | grep '"lat" : ' |cut -d " " -f13|tr -d ','|head -n1)
-LON=$(echo $DATA | grep '"lon" : ' |cut -d " " -f13|tr -d ','|head -n1)
-printf "$ORT: \"$LAT,$LON\"\n" >> ortsnameLatLon.txt
-printf 
-}
+' > ortsnameWDLatLon.geojson
 
+declare -A wikidata
+exit
 getLatLonByOrtViaWikidata () {
 IFS=\
 ;
 ORT=$1
 WD=$2
+TARGET=$3
 DATA=$(curl -L  https://www.wikidata.org/entity/$WD)
 LAT=$(echo $DATA | jq . |grep latitude | sed 's#.*"latitude": ##g'| sed 's#,##g#')
 LON=$(echo $DATA | jq . |grep longitude | sed 's#.*"longitude": ##g'| sed 's#,##g#')
@@ -31,26 +25,30 @@ printf '{
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [' >> ortsnameWDLatLon.geo.json
-printf "$LON,$LAT" >> ortsnameWDLatLon.geo.json
+        "coordinates": [' >> ortsnameWDLatLon.geojson
+printf "$LON,$LAT" >> ortsnameWDLatLon.geojson
 printf ' ]
       },
       "properties": {
-        "ort": "' >> ortsnameWDLatLon.geo.json
-printf "$ORT" >> ortsnameWDLatLon.geo.json
+        "ort": "' >> ortsnameWDLatLon.geojson
+printf "$ORT" >> ortsnameWDLatLon.geojson
 printf '",
-        "wd": "' >>ortsnameWDLatLon.geo.json
-printf "$WD" >>ortsnameWDLatLon.geo.json
+        "wd": "' >>ortsnameWDLatLon.geojson
+printf "$WD" >>ortsnameWDLatLon.geojson
+printf '",
+        "target": "' >>ortsnameWDLatLon.geojson
+printf "$TARGET" >>ortsnameWDLatLon.geojson
 printf '" }
     },
-' >>ortsnameWDLatLon.geo.json
+' >>ortsnameWDLatLon.geojson
 fi
 }
 
 getWikidataEntityAndLatLOn () {
 ORT=$1
+TARGET=$2
 WD=$(curl -G  https://www.wikidata.org/w/api.php --data-urlencode "action=wbgetentities" --data-urlencode "sites=dewiki" --data-urlencode "titles=$ORT" --data-urlencode "props=descriptions" --data-urlencode "languages=de" --data-urlencode "format=json"  |jq .|grep id|sed  's#.*"id": "##g' | sed 's#",##g' );
-getLatLonByOrtViaWikidata $ORT $WD
+getLatLonByOrtViaWikidata $ORT $WD $TARGET
 }
 ##
 # main
@@ -62,9 +60,12 @@ getLatLonByOrtViaWikidata $ORT $WD
 csvtool drop 1 content_export1567751077.csv > euregioHistoryWithoutFirstRow.csv
 IFS="
 ";
-for i in $(csvtool col 13 euregioHistoryWithoutFirstRow.csv | sed -s 's#"##g' | sed 's#, #\n#g' | sed 's#; #\n#g'| sed 's#-##g' | sort -u); do
-	if [ ! -z "$i" ]; then
+for i in $(csvtool col 13,17 euregioHistoryWithoutFirstRow.csv | sed -s 's#"##g' | sed 's#, #\n#g' | sed 's#; #\n#g'| sed 's#-##g' | sort); do
+	ort=$(echo "$i"|csvtool col 1 -)
+	target=$(echo "$i"|csvtool col 2 -)
+	if [ ! -z "$ort" -a ! -z "$target" ]; then
 		#getLatLonByOrt $i
-		getWikidataEntityAndLatLOn $i
+		echo $ort,$target
+		getWikidataEntityAndLatLOn $ort $target
 	fi
 done
