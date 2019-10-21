1eb7f1abcb02b9a4f501bf66fe81e14d87ba9d78
Pascal Christoph <christoph@hbz-nrw.de>
2019-10-21 13:50:05 +0200

    Consolidate

--- README.md
+++ README.md
@@ -3,19 +3,13 @@ Kartenvisualisierung der Datensätze von https://euregio-history.net/de
 
 Der [Datensatz wurde für codingDavinci in Dortmund 2019 hochgeladen](http://download.codingdavinci.de/index.php/s/5pimsCHErbWMfDs/download?path=%2F&files=content_export1567751077.csv).
 
+Zum [aktuellen Stand der Visualisierung](http://lobid.org/download/tmp/euregio-history/map.html).
+
 Dem Datensatz fehlen Geodaten für die Visualisierung. Die Ortsnamen liegen nur als Literale vor, deshalb bedarf es einer Disambiguierung, wenn diese Ortsnamen in einer
 Geodatenbank gesucht werden sollen, um an die Geodaten zu gelangen.
 
-Ein erster Ansatz, die nominatim der OpenStreetMap zu verwenden, hatte zuviele
-falsche Treffer geliefert.
-
-Der zweite Ansatz, dem Nachschlagen in lobid-nwbib (und damit der
-Eingrenzung des Suchraums auf Orte innerhalb von NRW) bringt lediglich [18 von
-68 Matches](https://github.com/dr0i/euregio-history-map/blob/master/ortsnameLatLon.txt).
-
 Der aktuelle Ansatz benutzt die wikidata API, um die Namen aufzulösen, und dann
-GeoDaten zu bekommen aus der Wikidata. Dieses Ergebnis hat ein paar Fehlmatches.
-Es gibt nun [44 von 67 Matches](https://github.com/dr0i/euregio-history-map/blob/master/ortsnameWDLatLon.txt).
-
-Ein dritter Ansatz wäre z.B. Pelias zu verwenden.
-Oder die Geodaten händisch zu recherchieren - so viele sind es ja nicht (68 -18 = 50).
+GeoDaten zu bekommen aus der Wikidata.
+Es gibt nun für 40 von 71 Orte GeoDaten.
+Dieses Ergebnis hat ein paar Fehlmatches.
+Weiter Infos dazu in #1.
--- src/main/java/CreateGeoJson.java
+++ src/main/java/CreateGeoJson.java
@@ -1,5 +1,3 @@
-package main.java;
-
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -22,214 +20,136 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.Response;
 
-//import com.fasterxml.jackson.databind.JsonNode;
-//import com.ning.http.client.AsyncHttpClient;
-
+/* Parses the euregio-history csv, mstches the region_places to wikidata entities, get the geo data from them, generates a geojson file out if.
+ * The output can be used like in "map.html" to visualize the data on a map.
+ * 
+ * @author: dr0i
+ */
 public class CreateGeoJson {
-	static HashMap<String, ArrayList<String>> ortUriMap = new HashMap<>();
-	static HashMap<String, ArrayList<String>> ortWdAndLatLonMap = new HashMap<>();
+    static HashMap<String, ArrayList<String>> ortUriMap = new HashMap<>();
+    static HashMap<String, ArrayList<String>> ortWdAndLatLonMap = new HashMap<>();
 
-	public static void main(String... args) {
-		CSVParser csvParser;
-		try {
-			csvParser = CSVFormat.DEFAULT
-					.parse(new InputStreamReader(new FileInputStream("content_export1567751077.csv")));
-			for (CSVRecord record : csvParser) {
-				String ort = record.get(12);
-				String idUri = record.get(16);
-				System.out.println(ort + "," + idUri);
-				String orte[];
-				if ((orte = ort.split("\\b")) != null) {
-					for (String o : orte) {
-						if (o.length() > 3) {
-							// ortUriMap.put(o,ortUriMap.get(o));
-							if (ortUriMap.containsKey(o))
-								ortUriMap.get(o).add(idUri);
-							else {
-								ArrayList<String> list = new ArrayList<String>();
-								list.add(idUri);
-								ortUriMap.put(o, list);
-							}
-							System.out.println(o + " =" + ortUriMap.get(o).toString());
-						}
-					}
-				}
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		for (String ort : ortUriMap.keySet()) {
-			String wdId;
-			if ((wdId = lookupWikidataGetId(ort)) != null) {
-				lookupWikidataParseGeoData(ort, wdId);
-			}
+    private static String getUriTitelHref(final String ID_URI, final String TITEL) {
+        return "</br><a href=\\\"" + ID_URI + "\\\">\\\"" + TITEL + "\\\"</a>";
+    }
 
-		}
-		// @formatter:off
-		String geoJsonHead=
-				"{\n" + 
-				"  \"type\": \"FeatureCollection\",\n" + 
-				"  \"features\": [\n";
-		String geoJsonEntry= //
-				"{\n" + 
-				"      \"type\": \"Feature\",\n" + 
-				"      \"geometry\": {\n" + 
-				"        \"type\": \"Point\",\n" + 
-				"        \"coordinates\": [%s,%s]\n" +
-				"      },\n" + 
-				"      \"properties\": {\n" + 
-				"        \"ort\": \"%s\",\n" +
-				"        \"wd\": \"%s\",\n" +
-						"        \"target\": \"%s\"\n" +
-				"}},"
-				;
-		// @formatter:on
-		
-	try {
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream("out.geojson"), StandardCharsets.UTF_8));
-			writer.write(geoJsonHead);
-		for (String ort : ortWdAndLatLonMap.keySet()) {
-			System.out.println(ort+","+ortWdAndLatLonMap.get(ort).toString());
-				writer.write(String.format(geoJsonEntry, ortWdAndLatLonMap.get(ort).get(1),
-						ortWdAndLatLonMap.get(ort).get(2), ort, ortWdAndLatLonMap.get(ort).get(0),
-						ortUriMap.get(ort).toString()));
-		}
-		writer.write("]}");
-			writer.close();
-		} catch (IOException e) {
-		e.printStackTrace();
-	}
-	}
+    public static void main(String... args) {
+        CSVParser csvParser;
+        try {
+            csvParser = CSVFormat.DEFAULT
+                    .parse(new InputStreamReader(new FileInputStream("content_export1567751077.csv")));
+            for (CSVRecord record : csvParser) {
+                String titel = record.get(3).trim();
+                String ort = record.get(12).trim();
+                String idUri = record.get(16).trim();
+                System.out.println(ort + "," + idUri);
+                String orte[];
+                if ((orte = ort.split("\\b")) != null) {
+                    for (String o : orte) {
+                        if (o.length() > 3) {
+                            // ortUriMap.put(o,ortUriMap.get(o));
+                            if (ortUriMap.containsKey(o))
+                                ortUriMap.get(o).add(getUriTitelHref(idUri, titel));
+                            else {
+                                ArrayList<String> list = new ArrayList<String>();
+                                list.add(getUriTitelHref(idUri, titel));
+                                ortUriMap.put(o, list);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (String ort : ortUriMap.keySet()) {
+            String wdId;
+            if ((wdId = lookupWikidataGetId(ort)) != null) {
+                lookupWikidataParseGeoData(ort, wdId);
+            }
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream("euregioHistory.geojson"), StandardCharsets.UTF_8));
+            writer.write(geoJsonHead);
+            boolean first = true;
+            for (String ort : ortWdAndLatLonMap.keySet()) {
+                if (!first)
+                    writer.write(",\n");
+                getJsonEntry(ort, writer);
+                first = false;
+            }
+            writer.write("]}");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Lookups a wikidata entity and, transform this to geo-nwbib-cache structure
-	 * and load into elasticsearch.
-	 * 
-	 * @param QID the wikidata Q-ID
-	 */
-	static String wdLookup = "https://www.wikidata.org/w/api.php?action=wbgetentities&sites=dewiki&titles=%s&props=descriptions&languages=de&format=json";
+    // @formatter:off
+    static String geoJsonHead = "{\n" + "  \"type\": \"FeatureCollection\",\n" + "  \"features\": [\n";
+    static String geoJsonEntry = //
+            "{\n" + "      \"type\": \"Feature\",\n" + "      \"geometry\": {\n" + "        \"type\": \"Point\",\n"
+                    + "        \"coordinates\": [%s,%s]\n" + "      },\n" + "      \"properties\": {\n"
+                    + "        \"ort\": \"%s\",\n" + "        \"wd\": \"%s\",\n" + "        \"target\": \"%s\"\n"
+                    + "}}";
 
-	public static String lookupWikidataGetId(final String ORT) {
-		String newWdUrl = String.format(wdLookup, ORT);
-		System.out.println("lookup:" + newWdUrl);
-		try (AsyncHttpClient client = new AsyncHttpClient()) {
-			JsonNode jnode = toApiResponseGet(client, newWdUrl);
-			JsonNode wdId = jnode.findValue("id");
-			return wdId.asText();
-		} catch (Exception e) {
-			System.out.println("Can't get wikidata entities ");
-		}
-		return null;
-	}
+    // @formatter:on
+    private static void getJsonEntry(final String ORT, final BufferedWriter WRITER) throws IOException {
+        System.out.println(ORT + "," + ortWdAndLatLonMap.get(ORT).toString());
+        WRITER.write(String.format(geoJsonEntry, ortWdAndLatLonMap.get(ORT).get(1), ortWdAndLatLonMap.get(ORT).get(2),
+                ORT, ortWdAndLatLonMap.get(ORT).get(0),
+                ortUriMap.get(ORT).toString().substring(1, ortUriMap.get(ORT).toString().length() - 1)));
+    }
 
-	private static final String HTTP_WWW_WIKIDATA_ORG_ENTITY = "http://www.wikidata.org/entity/";
+    /**
+     * Looks up a wikidata entity based on names of the locality
+     * 
+     * @param ORT
+     *            the name of the locality
+     */
+    static String wdLookup = "https://www.wikidata.org/w/api.php?action=wbgetentities&sites=dewiki&titles=%s&props=descriptions&languages=de&format=json";
 
-	public static void lookupWikidataParseGeoData(final String ORT, final String WDID) {
-		String newWdUrl = HTTP_WWW_WIKIDATA_ORG_ENTITY + WDID;
-		System.out.println("lookup:" + newWdUrl);
-		try (AsyncHttpClient client = new AsyncHttpClient()) {
-			JsonNode jnode = toApiResponseGet(client, newWdUrl);
-//			stream(jnode).map(transform2lobidWikidata()) //
-//					.forEach(index2Es());
-			String lat = jnode.findValue("latitude").asText();
-			String lon = jnode.findValue("longitude").asText();
+    public static String lookupWikidataGetId(final String ORT) {
+        String newWdUrl = String.format(wdLookup, ORT);
+        System.out.println("lookup:" + newWdUrl);
+        try (AsyncHttpClient client = new AsyncHttpClient()) {
+            JsonNode jnode = toApiResponseGet(client, newWdUrl);
+            JsonNode wdId = jnode.findValue("id");
+            return wdId.asText();
+        } catch (Exception e) {
+            System.out.println("Can't get wikidata entity for " + ORT);
+        }
+        return null;
+    }
 
-			System.out.println("lon,lat=" + lon + "," + lat);
-			ArrayList<String> list = new ArrayList<String>();
-			list.add(0, WDID);
-			list.add(1, lon);
-			list.add(2, lat);
-			ortWdAndLatLonMap.put(ORT, list);
-		} catch (Exception e) {
-			System.out.println("Can't get wikidata entities ");
-		}
-	}
+    private static final String HTTP_WWW_WIKIDATA_ORG_ENTITY = "http://www.wikidata.org/entity/";
 
-	private static final String JSON_ACCEPT_HEADER = "application/json";
+    public static void lookupWikidataParseGeoData(final String ORT, final String WDID) {
+        String newWdUrl = HTTP_WWW_WIKIDATA_ORG_ENTITY + WDID;
+        System.out.println("lookup:" + newWdUrl);
+        try (AsyncHttpClient client = new AsyncHttpClient()) {
+            JsonNode jnode = toApiResponseGet(client, newWdUrl);
+            String lat = jnode.findValue("latitude").asText();
+            String lon = jnode.findValue("longitude").asText();
+            ArrayList<String> list = new ArrayList<String>();
+            list.add(0, WDID);
+            list.add(1, lon);
+            list.add(2, lat);
+            ortWdAndLatLonMap.put(ORT, list);
+        } catch (Exception e) {
+            System.out.println("Can't get wikidata geo data for " + WDID);
+        }
+    }
 
-	private static JsonNode toApiResponseGet(final AsyncHttpClient CLIENT, final String API)
-			throws InterruptedException, ExecutionException, JsonParseException, JsonMappingException, IOException {
-		Thread.sleep(200); // be nice throttle down
-		Response response = CLIENT.prepareGet(API).setHeader("Accept", JSON_ACCEPT_HEADER).setFollowRedirects(true)
-				.execute().get();
-		return new ObjectMapper().readValue(response.getResponseBodyAsStream(), JsonNode.class);
-	}
+    private static final String JSON_ACCEPT_HEADER = "application/json";
 
-//	public static Function<JsonNode, Pair<String, JsonNode>> transform2lobidWikidata() {
-//		return node -> {
-//			ObjectNode root = null;
-//			String id;
-//			try {
-//				JsonNode geoNode = node.findPath("P625").findPath("mainsnak")
-//						.findPath("datavalue").findPath("value");
-//				ObjectMapper mapper = new ObjectMapper();
-//				root = mapper.createObjectNode();
-//				ObjectNode focus = mapper.createObjectNode();
-//				ObjectNode geo = mapper.createObjectNode();
-//				root.set(FOCUS, focus);
-//				JsonNode aliasesNode = node.findPath("aliases").findPath("de");
-//				if (!aliasesNode.isMissingNode())
-//					root.set("aliases", aliasesNode);
-//				ArrayNode type = mapper.createObjectNode().arrayNode();
-//				try {
-//					id = node.with("entities").fieldNames().next();
-//				} catch (NoSuchElementException ex) {
-//					id = node.findPath("id").asText();
-//				}
-//				focus.put("id", HTTP_WWW_WIKIDATA_ORG_ENTITY + id);
-//				root.put("id", NWBIB_SPATIAL_PREFIX + id);
-//				root.set("type", conceptNode);
-//				root.put("label",
-//						node.findPath("labels").findPath("de").findPath("value").asText());
-//				if (notationMap.containsKey(id))
-//					root.put("notation", notationMap.get(id));
-//				root.set("source", sourceNode);
-//				if (!geoNode.isMissingNode()
-//						&& !geoNode.findPath("latitude").isMissingNode()) {
-//					focus.set("geo", geo);
-//					geo.put("lat", geoNode.findPath("latitude").asDouble(0.0));
-//					geo.put("lon", geoNode.findPath("longitude").asDouble(0.0));
-//				} else {
-//					LOG.info("No geo coords for " + node.findPath("labels").findPath("de")
-//							.findPath("value").asText() + ": "
-//							+ node.findPath("id").asText());
-//				}
-//				String locatedInId = node.findPath("P131").findPath("mainsnak")
-//						.findPath("datavalue").findPath("value").findPath("id").asText();
-//				if (!locatedInId.isEmpty()) {
-//					JsonNode locatedInNode;
-//					if (locatedInMapCache.containsKey(locatedInId)) {
-//						locatedInNode = locatedInMapCache.get(locatedInId);
-//					} else {
-//						try (AsyncHttpClient client = new AsyncHttpClient()) {
-//							JsonNode jnode = toApiResponseGet(client,
-//									HTTP_WWW_WIKIDATA_ORG_ENTITY + locatedInId);
-//							String locatedInLabel = jnode.findPath("labels").findPath("de")
-//									.findPath("value").asText();
-//							LOG.debug("Found locatedIn id:" + locatedInId + " with label "
-//									+ locatedInLabel);
-//							locatedInNode = jnode.findPath("labels").findPath("de");
-//							locatedInMapCache.put(locatedInId, locatedInNode);
-//						}
-//					}
-//					root.set("locatedIn", locatedInNode);
-//				}
-//				List<JsonNode> typeNode = node.findValues("P31");
-//				if (!typeNode.isEmpty()) {
-//					typeNode.parallelStream()
-//							.forEach(e -> e.findValues("mainsnak")
-//									.forEach(e1 -> type.add(HTTP_WWW_WIKIDATA_ORG_ENTITY
-//											+ e1.findPath("datavalue").findPath("id").asText())));
-//					focus.set("type", type);
-//				}
-//				LOG.debug("Wikidata-Type extracted for type " + type.toString());
-//			} catch (Exception e) {
-//				LOG.error("Couldn't build a json document from the wd-entity ", e);
-//				return null;
-//			}
-//			return Pair.of(id, root);
-//		};
-//	}
+    private static JsonNode toApiResponseGet(final AsyncHttpClient CLIENT, final String API)
+            throws InterruptedException, ExecutionException, JsonParseException, JsonMappingException, IOException {
+        Thread.sleep(200); // be nice throttle down
+        Response response = CLIENT.prepareGet(API).setHeader("Accept", JSON_ACCEPT_HEADER).setFollowRedirects(true)
+                .execute().get();
+        return new ObjectMapper().readValue(response.getResponseBodyAsStream(), JsonNode.class);
+    }
+
 }
